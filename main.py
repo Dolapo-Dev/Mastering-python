4f3911fa1f4b4a660bc66f5ceda099f24fe3d4d7
Dolapo Obe <77324662+Dolapo-Dev@users.noreply.github.com>
2021-10-05 11:40:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+print("Hello world!\nHello world!")
+
+print("Hello" + " " + "Angela")
+print("Hello " + "Angela")
+print("Hello" + " Angela")
+input("where do you live ?")
+print("You typed " + input("where do you live ?"))
+# "input("where do you live ?")" will become what you type to the prompt, it can be otutput using print()
+print("Good to know you are from " + input("where are you from ?"))
+
+
+print("Hello " + input("what is your name ?"))
+
+print(len(input("what is your name ?")))
