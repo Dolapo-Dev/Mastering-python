7b24a0feaafb2bc35e32d4c9061abb57bd815597
Dolapo Obe <77324662+Dolapo-Dev@users.noreply.github.com>
2021-10-06 12:10:52 +0000

    Day 1 Completed

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ print( len( input("what is your name ? ") ) )
 name = "Jack"
 print(name)
 
+
 name = "Angela"
 print(name)
 
@@ -30,4 +31,32 @@ name = input("what is your name? ")
 length = len(name)
 
 print(length)
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+############################################
+# 🚨 Don't change the code above 👆
+####################################
+#Write your code below this line 👇
+c = a
+a = b
+b = c
+#Write your code above this line 👆
+####################################
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+##############################################################
+# Day 1 Challenge
+#1. Create a greeting for your program.
+print("Hello! Welcome to portal where Band names are generated")
+#2. Ask the user for the city that they grew up in.
+city = input("what is the name of the city you grew up?\n")
+#3. Ask the user for the name of a pet.
+pet = input("what is the name of your pet?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print('Your band name could be ' +city + " " + pet)
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
 
