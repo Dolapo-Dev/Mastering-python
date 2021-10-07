2f6d68018214031af4893b7878943693e8539b73
Dolapo Obe <77324662+Dolapo-Dev@users.noreply.github.com>
2021-10-07 11:14:13 +0000

    Third Day's work

---
+++ day3.py
@@ -0,0 +1,20 @@
+# First Quiz
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+  print(" Yaay! Please proceed to the rollercoaster")
+else:
+  print("I am sorry, you are not allowed to the rollercoaster")
+
+  # Second Quiz
+  # 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+if number % 2 == 0:
+  print(f"{number} is an even number")
+else:
+  print(f"{number} is an odd number")
+
